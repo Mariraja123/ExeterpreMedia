8bc8b3d9090ef7d58f2a0191828bd2490f1da883
Mariraja123 <59052760+Mariraja123@users.noreply.github.com>
2021-01-13 14:00:12 +0530

    Add files via upload

---
+++ replaceprogram.py
@@ -0,0 +1,24 @@
+import csv
+import re
+from datetime import datetime
+
+startTime = datetime.now()
+
+with open('t8.shakespeare.txt', 'r') as file :
+    filedata = file.read()
+  
+
+with open('french_dictionary.csv', 'r') as file,open('innovators.csv', 'w', newline='') as file1:
+    reader = csv.reader(file)
+    writer = csv.writer(file1)
+    writer.writerow(["English", "French", "Occurence"])
+    l=[]
+    for row in reader:
+        writer.writerow([row[0],row[1],filedata.count(row[0])])
+        src_str  = re.compile(row[0], re.IGNORECASE)
+        filedata = src_str.sub(row[1],filedata)
+
+with open('file.txt', 'w') as file:
+    file.write(filedata)
+  
+print("Time taken to execute this program : ",datetime.now()-startTime)
